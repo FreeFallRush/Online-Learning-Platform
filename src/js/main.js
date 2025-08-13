26f1f39dfd408615bc02944ab052a759b6fa3cb7
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-13 23:55:59 +0300

    Imported loadMap in main.js  from map.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
 import navbar from "./modules/navbar.js";
 import slider from "./modules/slider.js";
+import { loadMap } from "./modules/map.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   navbar();
   slider();
+
+  const map = document.getElementById("map-container");
+
+  if (map) {
+    loadMap();
+  }
 });
