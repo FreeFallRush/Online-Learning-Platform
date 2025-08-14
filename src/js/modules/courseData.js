bcd08a11dd7d6fa24631db96a0d66e4150ba926b
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-14 11:57:42 +0300

    Added courseData.js in src/js/modules
    Added fetchCourses function to fetch courses data from JSON file

---
+++ src/js/modules/courseData.js
@@ -0,0 +1,9 @@
+export async function fetchCourses() {
+  try {
+    const response = await fetch("data/coursesData.json");
+    return await response.json();
+  } catch (error) {
+    console.error("Failed to load courses data: ", error);
+    return [];
+  }
+}
