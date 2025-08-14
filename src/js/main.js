e1ad5f26c0d10d69feabf1337530f37a23911d61
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-14 12:18:56 +0300

    Imported initCourses and renderCourseDetail in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,24 @@
 import navbar from "./modules/navbar.js";
 import slider from "./modules/slider.js";
 import { loadMap } from "./modules/map.js";
+import { initCourses } from "./modules/courses.js";
+import { renderCourseDetail } from "./modules/courseDetails.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   navbar();
   slider();
 
   const map = document.getElementById("map-container");
+  const gallery = document.getElementById("gallery");
+  const courseDetails = document.getElementById("course-detail");
 
   if (map) {
     loadMap();
+  }
+
+  if (gallery) {
+    await initCourses();
+  } else if (courseDetails) {
+    await renderCourseDetail("course-detail");
   }
 });
